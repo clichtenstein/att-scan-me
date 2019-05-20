cc2a4b94d976d0af15568f11febeb737c98072f4
Slominsky, Ruty (rs282j) <rs282j@intl.att.com>
2019-05-20 12:30:49 +0300

    init my-att-scan-me app

--- src/components/wizard/sidebar-left/sidebar-left.jsx
+++ src/components/wizard/sidebar-left/sidebar-left.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import {MultiStep}  from 'react-multistep';
 
 class SidebarLeft extends Component {
     
     
     render() {
         const steps = [
-            {name: 'StepOne', component: <StepOne/>},
-            {name: 'StepTwo', component: <StepTwo/>},
-            {name: 'StepThree', component: <StepThree/>},
-            {name: 'StepFour', component: <StepFour/>}
+            // {name: 'StepOne', component: <StepOne/>},
+            // {name: 'StepTwo', component: <StepTwo/>},
+            // {name: 'StepThree', component: <StepThree/>},
+            // {name: 'StepFour', component: <StepFour/>}
           ];
         return (
             <div>
-                <Multistep showNavigation={true} steps={steps}/>
+                {/* <Multistep showNavigation={true} steps={steps}/> */}
             </div>
         );
     }
